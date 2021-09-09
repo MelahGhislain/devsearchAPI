6c81a26e9de4661b2efa95a1484ee9460679ad7b
MelahGhislain <melahghislain17@gmail.com>
2021-09-09 10:22:04 +0100

    created a requirement.txt

--- api/user/serializers.py
+++ api/user/serializers.py
@@ -13,11 +13,11 @@ class UserSerializer(serializers.ModelSerializer):
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        exclude = ('id', 'created',)
+        fields = '__all__'
 
 
 class SkillSerializer(serializers.ModelSerializer):
     class Meta:
         model = Skill
-        exclude = ('id', 'created',)
+        fields = '__all__'
 
