38ec16625d8f6b49077fceb5137159910d1085f5
MelahGhislain <melahghislain17@gmail.com>
2021-09-09 09:56:05 +0100

    created the some serializers

--- api/user/serializers.py
+++ api/user/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from .models import Profile, Skill
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -8,3 +9,15 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('username', 'email', 'password1', 'password2')
         
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        exclude = ('id', 'created',)
+
+
+class SkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
+        exclude = ('id', 'created',)
+
